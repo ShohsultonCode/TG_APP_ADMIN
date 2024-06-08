15d0f5505a040288839691bcbeb69da46daee783
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-08 14:08:04 +0500

    some

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,8 +6,6 @@ const Index = () => {
     const [products, setProducts] = useState([]);
     const [userId, setUserId] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [firstName, setFirstName] = useState("Unknown");
-    const [lastName, setLastName] = useState("Unknown");
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -30,25 +28,8 @@ const Index = () => {
             }
         };
 
-        const fetchUserInfo = async () => {
-            try {
-                const response = await fetch(`https://api.telegram.org/bot${YOUR_BOT_TOKEN}/getChat?chat_id=${userId}`);
-                const data = await response.json();
-                if (data && data.result) {
-                    const { first_name, last_name } = data.result;
-                    setFirstName(first_name || "Unknown");
-                    setLastName(last_name || "Unknown");
-                }
-            } catch (error) {
-                console.error('Error fetching user info:', error);
-            }
-        };
-
         fetchProducts();
-        if (userId) {
-            fetchUserInfo();
-        }
-    }, [userId]);
+    }, []);
 
     const handleDelete = async (productId) => {
         try {   
@@ -71,10 +52,7 @@ const Index = () => {
                 <h1 className="titlecha">Products:</h1>
                 <button onClick={() => navigate("/add/product")} className='btn btn-outline-success'>Add Product</button>
             </div>
-            <div>
-                <h3>First Name: {firstName}</h3>
-                <h3>Last Name: {lastName}</h3>
-            </div>
+            {userId && <p>User ID: {userId}</p>}
 
             {loading ? (
                 <Loader />
@@ -91,12 +69,6 @@ const Index = () => {
                                     <h6 className="card-subtitle mb-2 text-muted">Type: {product.product_category.category_name}</h6>
                                     <p className="card-text">Price: ${product.product_price.toFixed(2)}</p>
                                     <button className="btn btn-primary buttoncha">Order</button>
-                                    {userId === '5171708849' && (
-                                        <div className="mt-2">
-                                            <button onClick={() => handleEdit(product._id)} className="btn btn-warning me-2">Edit</button>
-                                            <button onClick={() => handleDelete(product._id)} className="btn btn-danger">Delete</button>
-                                        </div>
-                                    )}
                                 </div>
                             </div>
                         </div>
