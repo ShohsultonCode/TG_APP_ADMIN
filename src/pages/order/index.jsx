cc7fdb82659a3e4da3fc3f06b0cb944245b86cd1
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-08 17:04:22 +0500

    some

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -32,7 +32,7 @@ const Index = () => {
                 <Loader />
             ) : (
                 <div>
-                    <div className="d-flex justify-content-between align-items-center mb-4 backbutton">
+                    <div className="d-flex justify-content-between align-items-center">
                         <h1 className="titlecha">Orders:</h1>
                         <button type="button" className="btn btn-secondary" onClick={() => navigate('/')}>Back</button>
                     </div>
