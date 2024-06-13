640e8d25d0ba09f4227e0d29ce185610e7aa5a23
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-14 03:17:24 +0500

    add: last fucking commit

--- src/pages/order/index.jsx
+++ src/pages/order/index.jsx
@@ -12,7 +12,7 @@ const Index = () => {
     useEffect(() => {
         const fetchOrders = async () => {
             try {
-                const response = await fetch('https://botproject.uz/webappbot/api/orders');
+                const response = await fetch('https://botproject.uz/api/orders');
                 const data = await response.json();
                 setOrders(data);
                 setLoading(false);
@@ -52,7 +52,7 @@ const Index = () => {
                                     <td>{order?.order_product_id?.product_name}</td>
                                     <td>
                                         <img
-                                            src={`https://botproject.uz/webappbot/api/images/${order?.order_product_id?.product_image}`}
+                                            src={`https://botproject.uz/api/images/${order?.order_product_id?.product_image}`}
                                             alt={order?.order_product_id?.product_name}
                                             className="card-img-top img-fluid product-image"
                                             style={{ maxWidth: '100px' }}
--- src/pages/viewProduct/index.jsx
+++ src/pages/viewProduct/index.jsx
@@ -23,7 +23,7 @@ const Index = () => {
                 }
 
                 const productIds = selectedProducts.map(item => item.productId);
-                const response = await fetch('https://botproject.uz/webappbot/api/products/ask', {
+                const response = await fetch('https://botproject.uz/api/products/ask', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -73,7 +73,7 @@ const Index = () => {
                 order_count: productCounts[product._id]
             }));
 
-            const response = await fetch('https://botproject.uz/webappbot/api/orders/create', {
+            const response = await fetch('https://botproject.uz/api/orders/create', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -151,7 +151,7 @@ const Index = () => {
                             <td>{product.product_name}</td>
                             <td>
                                 <img
-                                    src={`https://botproject.uz/webappbot/api/images/${product.product_image}`}
+                                    src={`https://botproject.uz/api/images/${product.product_image}`}
                                     className="img-fluid"
                                     alt={product.product_name}
                                     style={{ height: "100px", width: "100px", objectFit: "cover" }}
